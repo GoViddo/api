417451f84f76b1496db0a833ed91b67aaa13eba0
GoViddo <contact@goviddo.com>
2019-08-24 20:29:22 +0530

    Send Mail Test

--- AppLayer/routes/index.js
+++ AppLayer/routes/index.js
@@ -2506,7 +2506,7 @@ module.exports = {
                 html: 'Embedded image: <img src="cid:unique@kreata.ee"/>',
                 attachments: [{
                     filename: 'go.png',
-                    path: 'https://goviddo.com/app/images/',
+                    path: 'https://goviddo.com/app/images',
                     cid: 'unique@kreata.ee' //same cid value as in the html img src
                 }]
             }
