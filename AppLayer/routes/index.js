2173c227792e98042a29811e0425998157e2f947
GoViddo <contact@goviddo.com>
2019-08-24 23:39:17 +0530

    Send Mail Test

--- AppLayer/routes/index.js
+++ AppLayer/routes/index.js
@@ -2397,7 +2397,7 @@ module.exports = {
             '<tbody>' +
             '<tr>'+
              '<td align="center" valign="top" width="150">&nbsp;</td>' +
-            '<td align="center" height="80" valign="middle" width="300"><a href="https://goviddo.com" target="_blank" ><img alt="" height="150" src="cid:unique@goviddo.igm" width="180" class="CToWUd"></a></td>' +
+            '<td align="center" height="80" valign="middle" width="300"><a href="https://goviddo.com" target="_blank" ><img alt="" height="75" src="cid:unique@goviddo.igm" width="150" class="CToWUd"></a></td>' +
             '<td align="right" valign="top" width="150">' +
             '<table border="0" cellpadding="0" cellspacing="0">' +
             '<tbody>' +
