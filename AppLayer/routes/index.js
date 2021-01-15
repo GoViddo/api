50369175835ce54ecf2518ea8d996deadf85861e
aarfaatech <aarfaatechnovision@gmail.com>
2021-01-16 02:10:07 +0530

    GoViddo API Updation URL

--- AppLayer/routes/index.js
+++ AppLayer/routes/index.js
@@ -1127,7 +1127,7 @@ module.exports = {
                                     cleosWalletUnlockQuery,
                                     function (err1, data1, stderr1) {
 
-                                        let sendTokens = "cleos -u https://eos.greymass.com/ push action hellogoviddo transfer '{\"from\":\"hellogoviddo\", \"to\":\"" + walletName + "\", \"quantity\":\"0.01 GOV\", \"memo\":\""+ memo +",\"hellogoviddo\"}' -p  hellogoviddo";
+                                        let sendTokens = "cleos -u https://eos.greymass.com/ push action hellogoviddo transfer '{\"from\":\"hellogoviddo\", \"to\":\"" + walletName + "\", \"quantity\":\"0.01 GOV\", \"memo\":\""+ memo +"\",\"hellogoviddo\"}' -p  hellogoviddo";
                                         console.log(sendTokens);
 
 
